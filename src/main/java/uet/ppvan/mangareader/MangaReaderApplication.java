5facbbf419bf1d9f2a83066de11e1f1734530eec
Phạm Văn Phúc <phuclaplace@gmail.com>
2022-10-31 14:02:40 +0700

    Change some Genre

--- src/main/java/uet/ppvan/mangareader/MangaReaderApplication.java
+++ src/main/java/uet/ppvan/mangareader/MangaReaderApplication.java
@@ -1,12 +1,18 @@
 package uet.ppvan.mangareader;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNullApi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import uet.ppvan.mangareader.mangas.enums.Genre;
+import uet.ppvan.mangareader.mangas.genres.GenreEntity;
+import uet.ppvan.mangareader.mangas.genres.GenreRepository;
 
 @SpringBootApplication
 @RestController
@@ -26,6 +32,18 @@ public class MangaReaderApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+
+    @Bean
+    @Autowired
+    public CommandLineRunner init(GenreRepository genreRepository) {
+        return args -> {
+            for (var genre: Genre.values()) {
+                var dbGenre = new GenreEntity();
+                dbGenre.setGenre(genre);
+                genreRepository.save(dbGenre);
             }
         };
     }
